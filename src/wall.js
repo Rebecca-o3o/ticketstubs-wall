91338c03bb7a745e2718af53a218805f6472e9e2
beccyo3o <rebecca.krummel@gmail.com>
2017-09-23 21:05:29 +0200

    add placeholder single ticket stub elements with random background colors

--- src/wall.js
+++ src/wall.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
 import axios from './axios';
 
 import {Menu} from './menu';
 import {Footer} from './footer';
+import {SingleTicketStub} from './single-ticket-stub';
+import {getTicketstubs, addTicketstub} from './actions';
 
 
 //===== components =====//
@@ -14,9 +17,43 @@ export class Wall extends React.Component{
         this.state = {};
     }
 
+    componentDidMount(){
+        this.props.dispatch(getTicketstubs());
+        console.log("Wall component did mount");
+    }
 
 
     render() {
+
+        const randomColor = () => {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        };
+
+        const {dispatch, getTicketstubs} = this.props;
+
+        const renderTicketstubs = () => {
+
+            return getTicketstubs.map(singleStub => {
+                return (
+                    <div>
+
+                        <div>
+                            <SingleTicketStub
+                                className="single-ticketstub"
+                                id={singleStub.id}
+                                imgsrc={singleStub.imgUrl}/>
+                        </div>
+
+                    </div>
+                );
+            });
+        };
+
 
         return (
 
@@ -28,7 +65,36 @@ export class Wall extends React.Component{
 
                 <div>
                     you are at your wall component
+                    <ul className="ticketstubs-wall-container">
+                        {/* {getTicketstubs && renderTicketstubs()} */}
+
+                        <li
+                            className="single-ticketstub"
+                            style={{backgroundColor: randomColor()}}>Rando
+                        </li>
+                        <li
+                            className="single-ticketstub"
+                            style={{backgroundColor: randomColor()}}>Rando
+                        </li>
+                        <li
+                            className="single-ticketstub"
+                            style={{backgroundColor: randomColor()}}>Rando
+                        </li>
+                        <li
+                            className="single-ticketstub"
+                            style={{backgroundColor: randomColor()}}>Rando
+                        </li>
+                        <li
+                            className="single-ticketstub"
+                            style={{backgroundColor: randomColor()}}>Rando
+                        </li>
+
+                    </ul>
                 </div>
+
+                <div>
+                </div>
+
 
                 <Footer />
 
@@ -36,3 +102,12 @@ export class Wall extends React.Component{
         );
     }
 }
+
+
+function mapStateToProps(state){
+    return {
+        getTicketstubs: state.getTicketstubs
+    };
+}
+
+export default connect(mapStateToProps)(Wall);
